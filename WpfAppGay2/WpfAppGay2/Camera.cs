7efd1098872a67f2300df5c49fad74cbf197ad2f
Pietro Rodaro <11pietro.rodaro@gmail.com>
2025-02-27 16:38:52 +0100

    Implementazione classe camera

---
+++ WpfAppGay2/WpfAppGay2/Camera.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppGay2
+{
+    public class Camera
+    {
+        //Variabili
+        private int numero;
+        private string descrizione;
+        private string tipologia;
+
+        public int Numero { get => numero; set => numero = value; }
+        public string Descrizione { get => descrizione; set => descrizione = value; }
+        public string Tipologia { get => tipologia; set => tipologia = value; }
+
+        // Costruttore
+        public Camera(int numero, string descrizione, string tipologia)
+        {
+            this.numero = numero;
+            this.descrizione = descrizione;
+            this.tipologia = tipologia;
+        }
+    }
+}
